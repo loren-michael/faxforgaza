d1813f2e54bf6b05c88850b52bedfe4f67223226
loren <lorenmichaelh@gmail.com>
2023-10-04 18:44:33 -0500

    fix navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,9 @@
 import "../App.css";
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div class="h-2">
+    <div>
       {/* <div class="w-full">
         <a href="/"><span class="font-merriweather font-semibold text-2xl h-40 float-left pl-5 text-white"> Loren Hartman </span></a>
         <span class="float-right">
@@ -17,7 +16,7 @@ const NavBar = () => {
 
       <nav class="flex items-center justify-between flex-wrap bg-hartmanblue-600 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
-          {/* <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg> */}
+          <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
           <span class="font-semibold font-merriweather text-xl tracking-tight"> Loren Hartman </span>
         </div>
         <div class="block lg:hidden">
@@ -33,9 +32,9 @@ const NavBar = () => {
           <a href="/resume" class="block mt-4 lg:inline-block lg:mt-0 text-hartmanblue-100 hover:text-white mr-4">
             Resume
           </a>
-          {/* <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
             Blog
-          </a> */}
+          </a>
         </div>
       <div>
         <a href="/resume" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-hartmanblue-600 hover:bg-white mt-4 lg:mt-0"> Resume </a>
