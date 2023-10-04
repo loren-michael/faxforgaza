105d3983c3d69c4d4179c9a610157b2d2923d688
loren <lorenmichaelh@gmail.com>
2023-10-04 17:51:54 -0500

    remove resume link, will revisit later

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,11 @@
-import { Link } from "react-router-dom";
 import "../App.css";
 import React from 'react'
+
 
 const Resume = () => {
   return (
     <div class="h-full bg-hartmanblue-200">
       <div>Resume</div>
-      <Link to="../resources/LorenHartmanResume.pdf" target="_blank" download> Download </Link>
     </div>
   )
 }
