76b69e2e029797806a18556ead7b877ab967023b
loren <lorenmichaelh@gmail.com>
2023-10-04 18:14:26 -0500

    navbar adjustments

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
   return (
     <div class="h-20 bg-blue-400">
       <div class="w-full">
-        <span class="float-left">NavBar left float</span>
+        <a href="/"><span class="font-merriweather text-2xl h-30 float-left pl-2"> Loren Hartman </span></a>
         <span class="float-right">
           <NavLink to="/about" href="/about" class="font-sans text-lg text-white"> About Me </NavLink>
           <NavLink to="/resume" href="/resume" class="font-sans text-lg text-white"> Resume </NavLink>
