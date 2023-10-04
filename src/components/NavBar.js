929958f13e9cea54cc2f99029015f68fc4bc643a
loren <lorenmichaelh@gmail.com>
2023-10-04 11:41:16 -0500

    add color to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,11 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div class="h-40">
+    <div class="h-40 bg-hartmanblue">
       <div class="w-full">
         <span class="float-left">Loren Hartman</span>
         <span class="float-right">
-          <NavLink to="/about" href="/about" class="font-sans text-lg text-black"></NavLink>
+          <NavLink to="/about" href="/about" class="font-sans text-lg text-white"></NavLink>
           <NavLink to="/resume" href="/resume" class="font-sans text-lg text-white"></NavLink>
         </span>
       </div>
