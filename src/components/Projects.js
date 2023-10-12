1894181cb121b294e5bc03e99bbb810cfd9026b9
loren <lorenmichaelh@gmail.com>
2023-10-12 17:13:42 -0500

    testing fix for youtube embeds

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,11 +4,6 @@ import YouTube, { YouTubeProps } from "react-youtube";
 
 const Projects = () => {
 
-  const onPlayerReady: YouTubeProps['onReady'] = (event) => {
-    // access to player in all event handlers via event.target
-    event.target.pauseVideo();
-  }
-
   // const opts: YouTubeProps['opts'] = {
   //   height: '390',
   //   width: '640',
@@ -17,6 +12,7 @@ const Projects = () => {
   //     autoplay: 1,
   //   },
   // };
+
 
   const options = {
     height: '390',
@@ -41,7 +37,7 @@ const Projects = () => {
                   <YouTube
                     videoId="4cvZbOYlLcg"
                     opts={options}
-                    onReady={onPlayerReady}
+                    onReady={this.pauseVideo()}
                     id="sellit video"
                   />
                   <p class="text-gray-700 font-merriweather">This app allows users to buy and sell audio gear. Simliar to eBay or Etsy. Users are authenticated, validations exist for all new items, and carts can be modified until the point of purchase.
@@ -55,7 +51,7 @@ const Projects = () => {
                   <YouTube 
                     videoId="NlN0V4Bq_yM" 
                     options={options} 
-                    onReady={onPlayerReady} 
+                    onReady={this.pauseVideo()} 
                     id="movie rental video"/>                
                   <p class="text-gray-700 font-merriweather">This app was meant to go a little deeper into my understanding of Ruby on Rails, while also applying useContext to access state from a central location. Also includes validations and authentication for users using bcrypt.
                   <br></br><br></br>
@@ -68,7 +64,7 @@ const Projects = () => {
                   <YouTube 
                     videoId="IPAvHJ6HqBk" 
                     options={options} 
-                    onReady={onPlayerReady} 
+                    onReady={this.pauseVideo()} 
                     id="character manager video"
                   />                
                   <p class="text-gray-700 font-merriweather">This simple app was designed to demonstrate knowledge of Ruby with Sinatra. It allows you to create and manage the levels of D&D characters.
@@ -81,7 +77,7 @@ const Projects = () => {
                   <YouTube 
                     videoId="7tJt0OAALhk" 
                     options={options} 
-                    onReady={onPlayerReady} 
+                    onReady={this.pauseVideo()} 
                     id="recipe v2 video"
                   />                 
                   <p class="text-gray-700 font-merriweather">The second app I made is another Recipe Manager - I guess I really wanted to expand on that idea! This time around, I utilized Semantic UI cards to my home page for a better display and also included the ability to "favorite" a recipe and leave a comment.
@@ -94,7 +90,7 @@ const Projects = () => {
                   <YouTube 
                     videoId="93D8D1Pqez8" 
                     options={options} 
-                    onReady={onPlayerReady} 
+                    onReady={this.pauseVideo()} 
                     id="recipe v1 video"
                   />                 
                   <p class="text-gray-700 font-merriweather">This was the first app that I build for a Flatiron project. It uses basic DOM manipulation to show a list of recipes from an included JSON database, and also allows you to add your own recipes to the list. The functionality is very basic, and does not include any validations or the ability to delete recipes without editing the database directly.
