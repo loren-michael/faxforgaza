68558ef01674031fd4c6c5f4edd5be3eed1dd45d
loren <lorenmichaelh@gmail.com>
2023-10-12 14:57:46 -0500

    adjusting project styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,7 +6,7 @@ const Projects = () => {
     <div>
       <div class="bg-blue-100">
         <div class="container mx-auto py-8">
-          <div class="grid grid-cols-4 sm:grid-cols-12 gap-6 px-4">
+          <div class="grid grid-cols-1 sm:grid-cols-12 gap-6 px-4">
             <div class="col-span-4 sm:col-span-3">
               <div class="bg-white shadow rounded-lg p-6">
                 <div class="col-span-4 sm:col-span-9">
