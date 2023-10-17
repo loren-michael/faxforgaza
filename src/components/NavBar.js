d89f44bdfbe084858b89fcc52f4121781d80bfe4
loren <lorenmichaelh@gmail.com>
2023-10-17 17:54:42 -0500

    remove nonfunctinoal hidden menu button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,11 +10,11 @@ const NavBar = () => {
           
         <span class="font-merriweather text-xl tracking-tight"> Loren Hartman </span>
       </div>
-      <div class="block lg:hidden">
+      {/* <div class="block lg:hidden">
         <button class="flex items-center px-3 py-2 border rounded text-blue-200 bg-blue-600 hover:text-white hover:border-white">
           <svg class="fill-current h-3 w-3 font-merriweather" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
         </button>
-      </div>
+      </div> */}
       <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div class="text-sm lg:flex-grow">
           <a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4 font-merriweather"><title>Home</title></a>
