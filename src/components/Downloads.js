f76011e6a3a97fa5813a3220a9f75f0ac48924ea
loren <lorenmichaelh@gmail.com>
2023-11-29 12:14:11 -0600

    move form to its own component, tracking down loop error

--- src/components/Downloads.js
+++ src/components/Downloads.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import logo from '../resources/logo.jpg'
 import RepDisplay from './RepDisplay';
+import Form from './Form';
 // import GovernorLetter from '../resources/GovernorLetter.pdf'
 // import HouseRepLetter from '../resources/HouseRepLetter.pdf'
 // import SenatorLetter from '../resources/SenatorLetter.pdf'
@@ -11,50 +12,50 @@ import RepDisplay from './RepDisplay';
 
 
 const Downloads = () => {
-    const repLookupURL = "https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=";
-    const [errors, setErrors] = useState([]);
-    const [name, setName] = useState("");
-    const [address, setAddress] = useState({
-        line1: "",
-        city: "",
-        state: "",
-        zip: ""
-    });
+    // const repLookupURL = "https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=";
+    // const [errors, setErrors] = useState([]);
+    // const [name, setName] = useState("");
+    // const [address, setAddress] = useState({
+    //     line1: "",
+    //     city: "",
+    //     state: "",
+    //     zip: ""
+    // });
     const [representatives, setRepresentatives] = useState({});
     
-    const resetForm = () => {
-        setName("")
-        setAddress({
-            line1: "",
-            city: "",
-            state: "",
-            zip: ""
-        })
-    }
+    // const resetForm = () => {
+    //     setName("")
+    //     setAddress({
+    //         line1: "",
+    //         city: "",
+    //         state: "",
+    //         zip: ""
+    //     })
+    // }
     
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const addressParams = "%" + address.line1.replaceAll(" ", "%20") + "%20" + address.city + "%20" + address.state + "%20" + address.state + "%20" + address.zip
-        console.log(repLookupURL + addressParams)
-        fetch(repLookupURL + addressParams + "&levels=country&levels=administrativeArea1&roles=legislatorUpperBody&roles=legislatorLowerBody&roles=headOfGovernment&key=AIzaSyB3B5EEu9oGWIZa8hIJKa1a2VxNcBZtoP4", {
-            method: "GET",
-            headers: {
-                "content-type": "application/json",
-                "accept": "application/json"
-            }
-        })
-        .then(r => {
-            if (r.ok) {
-                r.json()
-                .then(representatives => {
-                    setRepresentatives(representatives)
-                    console.log(representatives)
-                })
-            } else {
-                r.json().then(data => setErrors(data.errors))
-            }
-        })
-    }
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     const addressParams = "%" + address.line1.replaceAll(" ", "%20") + "%20" + address.city + "%20" + address.state + "%20" + address.state + "%20" + address.zip
+    //     console.log(repLookupURL + addressParams)
+    //     fetch(repLookupURL + addressParams + "&levels=country&levels=administrativeArea1&roles=legislatorUpperBody&roles=legislatorLowerBody&roles=headOfGovernment&key=AIzaSyB3B5EEu9oGWIZa8hIJKa1a2VxNcBZtoP4", {
+    //         method: "GET",
+    //         headers: {
+    //             "content-type": "application/json",
+    //             "accept": "application/json"
+    //         }
+    //     })
+    //     .then(r => {
+    //         if (r.ok) {
+    //             r.json()
+    //             .then(representatives => {
+    //                 setRepresentatives(representatives)
+    //                 console.log(representatives)
+    //             })
+    //         } else {
+    //             r.json().then(data => setErrors(data.errors))
+    //         }
+    //     })
+    // }
 
 
     return (
@@ -89,73 +90,7 @@ const Downloads = () => {
 
 
                     <div> 
-                        <h2 class="text-xl font-bold mt-6 mb-4">Enter your information to find your representatives:</h2>
-                        <div class="mb-6">
-                            <div class="flex justify-between">
-                                {errors.map(err => {
-                                    return (<p>{err.message}</p>)
-                                })}
-                                <form class={representatives ? "lookup-form" : "invisible"} className="lookup-information" onSubmit={handleSubmit}>
-                                    <div class="md:col-span-5">
-                                        <label htmlFor="name">Name: </label>
-                                        <input 
-                                            required
-                                            class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                                            type="text"
-                                            id="name"
-                                            value={ name }
-                                            onChange={ e => setName(e.target.value) }
-                                        />
-                                    </div>
-                                    <div class="md:col-span-5">
-                                        <label htmlFor="line1">Street: </label>
-                                        <input 
-                                            required
-                                            class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                                            type="text"
-                                            id="street"
-                                            value={ address.line1 }
-                                            onChange={ e => setAddress({...address, line1: e.target.value}) }
-                                        />
-                                    </div>
-                                    <div class="md:col-span-2">
-                                        <label htmlFor="line1">City: </label>
-                                        <input 
-                                            required
-                                            class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                                            type="text"
-                                            id="city"
-                                            value={ address.city }
-                                            onChange={ e => setAddress({...address, city: e.target.value}) }
-                                        />
-                                    </div>
-                                    <div class="md:col-span-1">
-                                        <label htmlFor="line1">State: </label>
-                                        <input 
-                                            required
-                                            class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                                            type="text"
-                                            id="state"
-                                            value={ address.state }
-                                            onChange={ e => setAddress({...address, state: e.target.value}) }
-                                        />
-                                    </div>
-                                    <div class="md:col-span-1">
-                                        <label htmlFor="line1">Zip Code: </label>
-                                        <input 
-                                            required
-                                            class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                                            type="text"
-                                            id="zip"
-                                            value={ address.zip }
-                                            onChange={ e => setAddress({...address, zip: e.target.value}) }
-                                        />
-                                    </div>
-                                    <button onClick={resetForm()} class="bg-blue-500 hover:bg-blue-700 text-white font-bold font-merriweather py-2 px-4 rounded"> Reset </button>
-                                    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold font-merriweather py-2 px-4 rounded"> Submit </button>
-                                </form>
-                            </div>
-                        </div>
+                        <Form representatives={representatives} setRepresentatives={setRepresentatives}  />
                     </div>
 
                     <hr class="my-6 border-t border-gray-300"></hr>
