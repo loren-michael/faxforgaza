f44e061d126c1375dbad08542197e8869f05d25f
loren <lorenmichaelh@gmail.com>
2023-12-07 11:51:35 -0600

    indentation adjustment

---
+++ src/components/RenderPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RenderPage() {
+  return (
+    <div>RenderPage</div>
+  )
+}
+
+export default RenderPage
--- src/components/RepDisplay.js
+++ src/components/RepDisplay.js
@@ -7,40 +7,40 @@ function RepDisplay({ userRepresentative, userSenators }) {
 
   return (
     <div>
-    <div class="bg-blue-100">
+      <div class="bg-blue-100">
         <div class="container mx-auto py-8">
-            <div class="grid grid-cols-4 sm:grid-cols-12 gap-6 px-4">
-                <div class="col-span-4 sm:col-span-3">
-                    <div class="bg-white shadow rounded-lg p-6">
-                        <div class="flex flex-col items-center">
-                            <img src={ logo } alt="Palestine" class="w-32 h-32 bg-gray-300 rounded-full mb-4 shrink-0"></img>
-                            <h1 class="text-xl font-merriweather font-bold">Fax for Gaza</h1>
-                            <p class="text-gray-600 font-merriweather">PDF files to use to contact your representatives in congress</p>
-                            <p class="text-gray-500 font-merriweather">Image by Alif Mauladani</p>
-                        </div>
+          <div class="grid grid-cols-4 sm:grid-cols-12 gap-6 px-4">
+            <div class="col-span-4 sm:col-span-3">
+              <div class="bg-white shadow rounded-lg p-6">
+                <div class="flex flex-col items-center">
+                  <img src={ logo } alt="Palestine" class="w-32 h-32 bg-gray-300 rounded-full mb-4 shrink-0"></img>
+                  <h1 class="text-xl font-merriweather font-bold">Fax for Gaza</h1>
+                  <p class="text-gray-600 font-merriweather">PDF files to use to contact your representatives in congress</p>
+                  <p class="text-gray-500 font-merriweather">Image by Alif Mauladani</p>
                 </div>
+              </div>
             </div>
             <div class="col-span-4 sm:col-span-9">
-                <div class="bg-white shadow rounded-lg p-6">
-                    <h2 class="text-xl font-bold mb-4 font-merriweather">Here are your state senators:</h2>
-                    {userSenators.map(senator => {
-                      return(
-                        <SenCard senator={senator} />
-                      )
-                    })}
+              <div class="bg-white shadow rounded-lg p-6">
+                <h2 class="text-xl font-bold mb-4 font-merriweather">Here are your state senators:</h2>
+                {userSenators.map(senator => {
+                  return(
+                    <SenCard senator={senator} />
+                  )
+                })}
 
-                    <h2 class="text-xl font-bold mb-4 font-merriweather">Here is your local representative:</h2>
-                    {userRepresentative.map(representative => {
-                      return (
-                        <RepCard representative={representative} />
-                      )
-                    })}
+                <h2 class="text-xl font-bold mb-4 font-merriweather">Here is your local representative:</h2>
+                {userRepresentative.map(representative => {
+                  return (
+                    <RepCard representative={representative} />
+                  )
+                })}
 
-                </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-    </div>
+      </div>
     </div>
 )
 }
