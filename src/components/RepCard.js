1e65906a69dbeccb0f71222a641c7b983f8eef76
loren <lorenmichaelh@gmail.com>
2023-12-07 11:45:46 -0600

    testing generate functionality

--- src/components/RepCard.js
+++ src/components/RepCard.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-function RepCard ({ representative, handleGenerate }) {
+function RepCard ({ representative }) {
+
+  function handleGenerate() {
+    console.log(representative)
+  }
 
   return (
     <div>
       <h3 class="text-l mb-4 font-merriweather pl-6">{representative.name}</h3>
-      <button onClick={handleGenerate(representative)}> Generate a letter </button>
+      <button onClick={handleGenerate}> Generate a letter </button>
     </div>
   )
 }
--- src/components/SenCard.js
+++ src/components/SenCard.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-function SenCard ({ senator, handleGenerate }) {
+function SenCard ({ senator }) {
+
+  function handleGenerate() {
+    console.log(senator)
+  }
 
   return (
     <div>
       <h3 class="text-l mb-4 font-merriweather pl-6">{senator.name}</h3>
-      <button onClick={handleGenerate(senator)}> Generate a letter </button>
+      <button onClick={handleGenerate}> Generate a letter </button>
     </div>
   )
 }
