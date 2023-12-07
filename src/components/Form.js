fff56b58221ccf01e23e19222891f4d04da1e86e
loren <lorenmichaelh@gmail.com>
2023-12-07 10:52:12 -0600

    indentation fix

--- src/components/Form.js
+++ src/components/Form.js
@@ -65,77 +65,73 @@ function Form ({ setUserSenators, setUserRepresentative }) {
 
     return (
         <div>
-        <h2 class="text-xl font-bold mt-6 mb-4">Enter your information to find your representatives:</h2>
-        <div class="mb-6">
-            <div class="flex justify-between">
-                {errors.map(err => {
-                    return (<p>{err.message}</p>)
-                })}
-                <form onSubmit={handleSubmit}>
-                    <div class="md:col-span-5">
-                        <label htmlFor="name">Name: </label>
-                        <input 
-                            required
-                            class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                            type="text"
-                            id="name"
-                            value={ name }
-                            onChange={ e => setName(e.target.value) }
-                        />
-                    </div>
-                    <div class="md:col-span-5">
-                        <label htmlFor="line1">Street: </label>
-                        <input 
-                            required
-                            class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                            type="text"
-                            id="line1"
-                            value={ address.line1 }
-                            onChange={ e => setAddress({...address, line1: e.target.value}) }
-                        />
-                    </div>
-                    <div class="md:col-span-2">
-                        <label htmlFor="city">City: </label>
-                        <input 
-                            required
-                            class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                            type="text"
-                            id="city"
-                            value={ address.city }
-                            onChange={ e => setAddress({...address, city: e.target.value}) }
-                        />
-                    </div>
-                    <div class="md:col-span-1">
-                        <label htmlFor="state">State: </label>
-                        <input 
-                            required
-                            class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                            type="text"
-                            id="state"
-                            value={ address.state }
-                            onChange={ e => setAddress({...address, state: e.target.value}) }
-                        />
-                    </div>
-                    <div class="md:col-span-1">
-                        <label htmlFor="zip">Zip Code: </label>
-                        <input 
-                            required
-                            class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                            type="text"
-                            id="zip"
-                            value={ address.zip }
-                            onChange={ e => setAddress({...address, zip: e.target.value}) }
-                        />
-                    </div>
-                    <button onClick={resetForm} class="bg-blue-500 hover:bg-blue-700 text-white font-bold font-merriweather p-2 rounded"> Reset </button>  
-                    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold font-merriweather p-2 rounded"> Submit </button>
-                </form>
+            <h2 class="text-xl font-bold mt-6 mb-4">Enter your information to find your representatives:</h2>
+            <div class="mb-6">
+                <div class="flex justify-between">
+                    {errors.map(err => {
+                        return (<p>{err.message}</p>)
+                    })}
+                    <form onSubmit={handleSubmit}>
+                        <div class="md:col-span-5">
+                            <label htmlFor="name">Name: </label>
+                            <input 
+                                required
+                                class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                                type="text"
+                                id="name"
+                                value={ name }
+                                onChange={ e => setName(e.target.value) }
+                            />
+                        </div>
+                        <div class="md:col-span-5">
+                            <label htmlFor="line1">Street: </label>
+                            <input 
+                                required
+                                class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                                type="text"
+                                id="line1"
+                                value={ address.line1 }
+                                onChange={ e => setAddress({...address, line1: e.target.value}) }
+                            />
+                        </div>
+                        <div class="md:col-span-2">
+                            <label htmlFor="city">City: </label>
+                            <input 
+                                required
+                                class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                                type="text"
+                                id="city"
+                                value={ address.city }
+                                onChange={ e => setAddress({...address, city: e.target.value}) }
+                            />
+                        </div>
+                        <div class="md:col-span-1">
+                            <label htmlFor="state">State: </label>
+                            <input 
+                                required
+                                class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                                type="text"
+                                id="state"
+                                value={ address.state }
+                                onChange={ e => setAddress({...address, state: e.target.value}) }
+                            />
+                        </div>
+                        <div class="md:col-span-1">
+                            <label htmlFor="zip">Zip Code: </label>
+                            <input 
+                                required
+                                class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                                type="text"
+                                id="zip"
+                                value={ address.zip }
+                                onChange={ e => setAddress({...address, zip: e.target.value}) }
+                            />
+                        </div>
+                        <button onClick={resetForm} class="bg-blue-500 hover:bg-blue-700 text-white font-bold font-merriweather p-2 rounded"> Reset </button>  
+                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold font-merriweather p-2 rounded"> Submit </button>
+                    </form>
+                </div>
             </div>
-            {/* <div>
-                <h2 class="text-xl font-bold mt-6 mb-4">Your representatives based on your address:</h2>
-                    {representatives ? <RepDisplay representatives={representatives} /> : <p>Please fill out the form above to find your representatives.</p>}
-            </div> */}
-        </div>
         </div>
     )
 }
