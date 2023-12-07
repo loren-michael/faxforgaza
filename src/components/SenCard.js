53c478b141feb20f9f789361cb1876bf078b6a99
loren <lorenmichaelh@gmail.com>
2023-12-07 13:12:42 -0600

    added font changes to repcard, duplicated changes to sencard

--- src/components/SenCard.js
+++ src/components/SenCard.js
@@ -7,9 +7,19 @@ function SenCard ({ senator }) {
   }
 
   return (
-    <div>
-      <h3 class="text-l mb-4 font-merriweather pl-6">{senator.name}</h3>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold font-merriweather p-2 rounded" onClick={handleGenerate}> Generate a letter </button>
+    <div class="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+      <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 font-merriweather">
+        {senator.name}
+      </h5>
+      <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200 font-merriweather">
+        <img src={senator.photoUrl} alt="Official Portrait"/>
+        {senator.party}
+      </p>
+      <button 
+        type="button"
+        onClick={handleGenerate}
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold font-merriweather p-2 rounded" data-te-ripple-init data-te-ripple-color="light"
+      > Generate Letter </button>
     </div>
   )
 }
