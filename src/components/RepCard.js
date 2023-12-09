ba29083e0389cab4cf79305a83835ed93c7dac2d
loren <lorenmichaelh@gmail.com>
2023-12-09 09:40:04 -0600

    adding justify to card elements

--- src/components/RepCard.js
+++ src/components/RepCard.js
@@ -7,18 +7,18 @@ function RepCard ({ representative }) {
   }
 
   return (
-    <div class="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 justify-center">
+    <div class="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
       <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 font-merriweather">
         {representative.name}
       </h5>
-      <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200 font-merriweather">
-        {representative.photoUrl ? <img src={representative.photoUrl} alt="Official Portrait"/> : <></>}
+      <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200 font-merriweather justify-center">
+        {representative.photoUrl ? <img src={representative.photoUrl} alt="Official Portrait" class="justify-center"/> : <></>}
         {representative.party}
       </p>
       <button 
         type="button"
         onClick={handleGenerate}
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold font-merriweather p-2 rounded" data-te-ripple-init data-te-ripple-color="light"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold font-merriweather p-2 rounded justify-center" data-te-ripple-init data-te-ripple-color="light"
       > Generate Letter </button>
     </div>
   )
--- src/components/SenCard.js
+++ src/components/SenCard.js
@@ -8,17 +8,17 @@ function SenCard ({ senator }) {
 
   return (
     <div class="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 justify-center">
-      <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 font-merriweather">
+      <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 font-merriweather justify-center">
         {senator.name}
       </h5>
       <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200 font-merriweather">
-        {senator.photoUrl ? <img src={senator.photoUrl} alt="Official Portrait"/> : <></>}
+        {senator.photoUrl ? <img src={senator.photoUrl} class="justify-center" alt="Official Portrait"/> : <></>}
         {senator.party}
       </p>
       <button 
         type="button"
         onClick={handleGenerate}
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold font-merriweather p-2 rounded" data-te-ripple-init data-te-ripple-color="light"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold font-merriweather p-2 rounded justify-center" data-te-ripple-init data-te-ripple-color="light"
       > Generate Letter </button>
     </div>
   )
