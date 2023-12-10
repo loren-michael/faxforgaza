77cec5f9abcda132bb1bf1ceb01a69ac1bc81182
loren <lorenmichaelh@gmail.com>
2023-12-10 14:30:54 -0600

    add contexts to rep letter

--- src/components/RepLetter.js
+++ src/components/RepLetter.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
-import { StoreContext } from '../context/SenatorStore';
+import { NameContext } from '../context/NameStore';
+import { AddressContext } from '../context/AddressStore';
+import { OfficialContext } from '../context/OfficialStore';
 import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
 
 const styles = StyleSheet.create({
@@ -15,7 +17,9 @@ const styles = StyleSheet.create({
 });
 
 function RepLetter() {
-  const [name, address, official] = useContext(StoreContext);
+  const [name, setName] = useContext(NameContext);
+  const [address, setAddress] = useContext(AddressContext);
+  const [official, setOfficial] = useContext(OfficialContext);
 
   return (
     <Document>
