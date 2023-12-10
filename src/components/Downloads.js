c7de239f2b7df1c83cd53b3dec774ef807da51a4
loren <lorenmichaelh@gmail.com>
2023-12-09 21:33:39 -0600

    remove header from main downloads page

--- src/components/Downloads.js
+++ src/components/Downloads.js
@@ -23,7 +23,7 @@ function Downloads () {
                             <div class="bg-white shadow rounded-lg p-6">
                                 <div class="flex flex-col items-center">
                                     <img src={ logo } alt="Palestine" class="w-32 h-32 bg-gray-300 rounded-full mb-4 shrink-0"></img>
-                                    <h1 class="text-xl font-merriweather font-bold">Fax for Gaza</h1>
+                                    {/* <h1 class="text-xl font-merriweather font-bold">Fax For Gaza</h1> */}
                                     <p class="text-gray-600 font-merriweather">PDF files to use to contact your representatives in congress</p>
                                     <p class="text-gray-500 font-merriweather">Image by Alif Mauladani</p>
                                 </div>
