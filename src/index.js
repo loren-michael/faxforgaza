95aabf423ef08a127acc2062d2a52feb3f59077d
loren <lorenmichaelh@gmail.com>
2023-12-09 21:19:27 -0600

    bug: new attempt to fix render issue

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import * as ReactDOMClient from 'react-dom/client';
-import { createRoot } from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
 import "./App.css";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { StoreProvider } from './context/store';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StoreProvider>
+      <App/>
+    </StoreProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -16,16 +25,16 @@ import { StoreProvider } from './context/store';
 // , document.getElementById('root'));
 
 
-const container = ReactDOM.createRoot(document.getElementById('root'));
-const root = createRoot(container)
+// const container = ReactDOM.createRoot(document.getElementById('root'));
+// const root = createRoot(container)
 
-root.render(
-  <React.StrictMode>
-    <StoreProvider>
-      <App />
-    </StoreProvider>
-  </React.StrictMode>
-)
+// root.render(
+//   <React.StrictMode>
+//     <StoreProvider>
+//       <App />
+//     </StoreProvider>
+//   </React.StrictMode>
+// )
 
 // root.render(
 //   <React.StrictMode>
