0525384dd7ac4253d2b713fb8a9b843e1d92ca72
loren <lorenmichaelh@gmail.com>
2023-12-11 10:14:43 -0600

    addinf flex/flexcol to repcard to test display

--- src/components/RepCard.js
+++ src/components/RepCard.js
@@ -15,11 +15,11 @@ function RepCard ({ representative }) {
 
   return (
     <div class="rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 flex flex-col">
-      <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 font-merriweather">
+      <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 font-merriweather flex flex-col">
         {representative.name}
       </h5>
-      <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200 font-merriweather justify-center">
-        {representative.photoUrl ? <img src={representative.photoUrl} alt="Official Portrait" class="justify-center"/> : <></>}
+      <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200 font-merriweather justify-center flex flex-col">
+        {representative.photoUrl ? <img src={representative.photoUrl} alt="Official Portrait" class="justify-center flex flex-col"/> : <></>}
         {representative.party}
       </p>
       <button 
