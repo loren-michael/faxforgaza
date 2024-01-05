e3a0d1ef19bc978b5140f904c4cf2b885b498ba5
loren <lorenmichaelh@gmail.com>
2024-01-05 13:58:47 -0600

    remove flex flexcol from parent div

--- src/components/RepCard.js
+++ src/components/RepCard.js
@@ -20,7 +20,7 @@ function RepCard ({ representative }) {
   }
 
   return (
-    <div class="flex flex-col rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+    <div class="rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
       <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 font-merriweather">
         {representative.name}
       </h5>
