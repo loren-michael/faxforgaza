f55d47a9de946a7e6d893ad2b193211ada396085
loren <lorenmichaelh@gmail.com>
2024-01-05 13:46:39 -0600

    remove senator card distinction from previous test

--- src/components/SenCard.js
+++ src/components/SenCard.js
@@ -52,7 +52,7 @@ function SenCard ({ senator }) {
   return (
     <div class="flex flex-col rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
       <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 font-merriweather">
-        {senator.name} Senator Card
+        {senator.name}
       </h5>
       <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200 font-merriweather">
         {senator.photoUrl ? <img src={senator.photoUrl} alt="Official Portrait"/> : <></>}
