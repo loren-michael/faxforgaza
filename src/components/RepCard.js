3786e02cd99f0e1f75a60e9b41ecbe3197f87590
loren <lorenmichaelh@gmail.com>
2024-01-05 18:27:17 -0600

    remove prop from blob function

--- src/components/RepCard.js
+++ src/components/RepCard.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import RepLetter from './RepLetter';
-import FileSaver from 'file-saver';
+import { saveAs } from 'file-saver';
+import { pdf } from '@react-pdf/renderer';
 
 function RepCard ({ representative }) {
 
-  function handleDownload() {
-    
-  }
+  const handleDownload = () => {
+    const downloadPdf = async () => {
+      const fileName = `${representative.name}Letter.pdf`;
+      const blob = await pdf(<RepLetter official={representative} />).toBlob();
+      saveAs(blob, fileName)
+    };
+    console.log("download pdf")
+    downloadPdf();
+  };
 
   return (
     <div class="rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
--- src/components/SenCard.js
+++ src/components/SenCard.js
@@ -15,12 +15,12 @@ function SenCard ({ senator }) {
   const handleDownload = () => {
     const downloadPdf = async () => {
       const fileName = `${senator.name}Letter.pdf`;
-      const blob = await pdf(<SenLetter official={senator} />).toBlob();
+      const blob = await pdf(<SenLetter />).toBlob();
       saveAs(blob, fileName)
     };
     console.log("download pdf")
     downloadPdf();
-  }
+  };
 
   return (
     <div class="rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
