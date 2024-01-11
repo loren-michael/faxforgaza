9f54c6911d59740a540c7d9d76d80290c85cfb0b
loren <lorenmichaelh@gmail.com>
2024-01-10 22:59:10 -0600

    filling out links page with filler code to modify later

--- src/components/Links.js
+++ src/components/Links.js
@@ -19,9 +19,24 @@ function Links() {
           </div>
           <div class="col-span-4 sm:col-span-9">
               <div class="bg-white shadow rounded-lg p-6">
-                {/* { userRepresentative ? userRepresentative.map(representative => {return (<RepCard representative={representative} />)}) : <></> } */}
-                {/* { userSenators ? userSenators.map(senator => {return (<SenCard senator={senator}/>)}) : <></>} */}
-                
+                <h2 class="text-xl font-bold mb-4 font-merriweather">Fax For Gaza</h2>
+                  <p class="text-gray-700 font-merriweather">
+                  Fax for Gaza makes it a little easier to contact your representatives to support a ceasefire in Palestine by providing PDF files that you can fax for free to your representatives using FaxZero.com
+                  <br></br><br></br>
+                  It is our hope that if people are given these resources and links they will be more likely to utilize them daily to flood our representatives with faxes directly from their phones.
+                  <br></br><br></br>
+                  To use this resource, you can either use our form to find your representatives by clicking the button below, or, if you know the names of your representatives or would rather not use our form, you can download our blank form PDF letters below and enter in your information on your device to fax. Once you have your PDF files, go to FaxZero.com using the link above, find your representatives, upload your PDF and send it!
+                  <br></br><br></br>
+                  This site uses Google's Civic Information API and does NOT save any information about visitors.
+                  <br></br>
+                  To share any feedback, please write to: faxforgaza at gmail dot com
+                  </p>
+                  {/* Light gray line breaks */}
+                  {/* <hr class="my-6 border-t border-gray-300"></hr> */}
+                  {/* <hr class="my-6 border-t border-gray-300"></hr> */}
+                  {/* <hr class="my-6 border-t border-gray-300"></hr> */}
+                  {/* <hr class="my-6 border-t border-gray-300"></hr> */}
+                  {/* <hr class="my-6 border-t border-gray-300"></hr> */}
               </div>
           </div>
         </div>
