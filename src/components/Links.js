ffadbe247e69f489e2c8e574b9cea84b27b84b25
loren <lorenmichaelh@gmail.com>
2024-01-10 23:42:54 -0600

    building links list

--- src/components/Links.js
+++ src/components/Links.js
@@ -20,9 +20,100 @@ function Links() {
           <div class="col-span-4 sm:col-span-9">
               <div class="bg-white shadow rounded-lg p-6">
                 <h2 class="text-xl font-bold mb-4 font-merriweather">Other Helpful Links</h2>
+                <br></br>
+                <br></br>
+{/* House Summary */}
+                  <div class="mb-6">
+                    <div class="flex justify-between">
+                      <span class="text-blue-500 font-bold">House Resolution 786 Summary</span>
+                      <span>
+                        <div>
+                          <a href="https://www.congress.gov/bill/118th-congress/house-resolution/786" class="bg-blue-500 hover:bg-blue-700 text-white font-merriweather font-bold py-2 px-4 rounded"> Go </a>
+                        </div>
+                      </span>
+                    </div>
+                    <p class="mt-2">This link takes you to the official summary of House Resolution 786 introduced by Cori Bush (D-MO) on October 16, 2023.
+                    </p>
+                  </div>
+
+                  <hr class="my-6 border-t border-gray-300"></hr>
+{/* Co-Sponsors */}
+                  <div class="mb-6">
+                    <div class="flex justify-between">
+                      <span class="text-blue-500 font-bold">House Resolution 786 Co-Sponsors</span>
+                      <span>
+                        <div>
+                          <a href="https://www.congress.gov/bill/118th-congress/house-resolution/786/cosponsors" class="bg-blue-500 hover:bg-blue-700 text-white font-merriweather font-bold py-2 px-4 rounded"> Go </a>
+                        </div>
+                      </span>
+                    </div>
+                    <p class="mt-2">This link takes you to the current list of co-sponsors of H. Res. 786. Check this list to see if your representative is a co-sponsor of the bill.
+                    </p>
+                  </div>
+
+                  <hr class="my-6 border-t border-gray-300"></hr>
+{/* BDS Movement */}
+                  <div class="mb-6">
+                    <div class="flex justify-between">
+                      <span class="text-blue-500 font-bold">BDS Movement</span>
+                      <span>
+                        <div>
+                          <a href="https://bdsmovement.net/" class="bg-blue-500 hover:bg-blue-700 text-white font-merriweather font-bold py-2 px-4 rounded"> Go </a>
+                        </div>
+                      </span>
+                    </div>
+                    <p class="mt-2">The Boycott, Divestment, Sanctions (BDS) movement works to end international support for Israel's oppression of Palestinians and pressure Israel to comply with international law.
+                    </p>
+                  </div>
+
+                  <hr class="my-6 border-t border-gray-300"></hr>
+{/* JVP */}
+                  <div class="mb-6">
+                    <div class="flex justify-between">
+                      <span class="text-blue-500 font-bold">Jewish Voice for Peace</span>
+                      <span>
+                        <div>
+                          <a href="https://www.jewishvoiceforpeace.org/" class="bg-blue-500 hover:bg-blue-700 text-white font-merriweather font-bold py-2 px-4 rounded"> Go </a>
+                        </div>
+                      </span>
+                    </div>
+                    <p class="mt-2">Jewish Voice for Peace is the largest progresive Jewish anti-Zionist organization in the world and has organized countless demonstrations around the world over the past few months.
+                    </p>
+                  </div>
+
+                  {/* <hr class="my-6 border-t border-gray-300"></hr>
+
+                  <div class="mb-6">
+                    <div class="flex justify-between">
+                      <span class="text-blue-500 font-bold">House Resolution 786 Co-Sponsors</span>
+                      <span>
+                        <div>
+                          <a href="https://www.congress.gov/bill/118th-congress/house-resolution/786/cosponsors" class="bg-blue-500 hover:bg-blue-700 text-white font-merriweather font-bold py-2 px-4 rounded"> Go </a>
+                        </div>
+                      </span>
+                    </div>
+                    <p class="mt-2">This link takes you to the current list of co-sponsors of H. Res. 786. Check this list to see if your representative is a co-sponsor of the bill.
+                    </p>
+                  </div> */}
+
+                  {/* <hr class="my-6 border-t border-gray-300"></hr>
+
+                  <div class="mb-6">
+                    <div class="flex justify-between">
+                      <span class="text-blue-500 font-bold">House Resolution 786 Co-Sponsors</span>
+                      <span>
+                        <div>
+                          <a href="https://www.congress.gov/bill/118th-congress/house-resolution/786/cosponsors" class="bg-blue-500 hover:bg-blue-700 text-white font-merriweather font-bold py-2 px-4 rounded"> Go </a>
+                        </div>
+                      </span>
+                    </div>
+                    <p class="mt-2">This link takes you to the current list of co-sponsors of H. Res. 786. Check this list to see if your representative is a co-sponsor of the bill.
+                    </p>
+                  </div> */}
+
+                  {/* <hr class="my-6 border-t border-gray-300"></hr> */}
+
                   {/* Light gray line breaks */}
-                  {/* <hr class="my-6 border-t border-gray-300"></hr> */}
-                  {/* <hr class="my-6 border-t border-gray-300"></hr> */}
                   {/* <hr class="my-6 border-t border-gray-300"></hr> */}
                   {/* <hr class="my-6 border-t border-gray-300"></hr> */}
                   {/* <hr class="my-6 border-t border-gray-300"></hr> */}
