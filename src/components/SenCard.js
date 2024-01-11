51c62814ff7d62933b1bbb3b94aaa33a444f175b
loren <lorenmichaelh@gmail.com>
2024-01-10 19:31:12 -0600

    GOOD WORKING VERSION add line break before buttons on sen card

--- src/components/SenCard.js
+++ src/components/SenCard.js
@@ -84,6 +84,7 @@ function SenCard ({ senator }) {
           </Document>
         </PDFViewer>
       </div>
+      <br></br>
       <button onClick={e => handleDownload(e)} type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold font-merriweather p-2 rounded justify-center" data-te-ripple-init data-te-ripple-color="light"> Download Letter </button>
     </div>
   )
